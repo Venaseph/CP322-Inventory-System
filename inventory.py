6ff826caeb714309533c857f6205b0a77614a3d4
C.Peragine <cperagine@gmail.com>
2018-04-08 11:33:10 -0400

    Start on print

--- inventory.py
+++ inventory.py
@@ -1,17 +1,16 @@
 import sys
 import re
 
+# Can't use octals for key values, so strings, init dicts
+parts = {'0001': 'Wireless Mouse', '0002': 'Wireless Keyboard', '0003': '19" Monitor', '0004': '23" Monitor', '0005'
+         : 'HDMI Cable', '0006': 'VGA Cable', '0007': 'USB Cable', '0008': 'Power Cable', '0009': '8GB Thumb Drive',
+         '0010': '16GB Thumb Drive'}
+descr = {'0001': 3, '0002': 3, '0003': 2, '0004': 2, '0005': 5, '0006': 5, '0007': 10, '0008': 5, '0009': 3, '0010': 4}
+
 
 def main():
     # do/while control
     run = True
-
-    # Can't use octals for key values, so strings, init dicts
-    parts = {'0001': 'Wireless Mouse', '0002': 'Wireless Keyboard', '0003': '19" Monitor', '0004': '23" Monitor', '0005'
-             : 'HDMI Cable', '0006': 'VGA Cable', '0007': 'USB Cable', '0008': 'Power Cable', '0009': '8GB Thumb Drive',
-             '0010': '16GB Thumb Drive'}
-    descr = {'0001': 3, '0002': 3, '0003': 2, '0004': 2, '0005': 5, '0006': 5, '0007': 10, '0008': 5, '0009': 3, '0010':
-             4}
 
     while run:
         # print menu to screen and gather user selection
@@ -43,7 +42,10 @@ def defswitcher(selection, run):
 
 
 def printall():
-    print("printall")
+    # passing global to defs to avoid creating a new box for Schrödinger each run though
+    global parts, descr
+
+    print("  ".join())
 
 
 def addpart():
