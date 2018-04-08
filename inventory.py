d6c7c3eaaf7be3b25dced14389fcb9a5bc0af298
C.Peragine <cperagine@gmail.com>
2018-04-08 18:14:07 -0400

    Changes to flow control for main menu

--- inventory.py
+++ inventory.py
@@ -3,7 +3,7 @@ import re
 
 # Can't use octals for key values, so strings, init dicts
 parts = {'0001': 'Wireless Mouse', '0002': 'Wireless Keyboard', '0003': '19" Monitor', '0004': '23" Monitor', '0005'
-         : 'HDMI Cable', '0006': 'VGA Cable', '0007': 'USB Cable', '0008': 'Power Cable', '0009': '8GB Thumb Drive',
+: 'HDMI Cable', '0006': 'VGA Cable', '0007': 'USB Cable', '0008': 'Power Cable', '0009': '8GB Thumb Drive',
          '0010': '16GB Thumb Drive'}
 descr = {'0001': 3, '0002': 3, '0003': 2, '0004': 2, '0005': 5, '0006': 5, '0007': 10, '0008': 5, '0009': 3, '0010': 4}
 
@@ -16,10 +16,32 @@ def main():
         # print menu to screen and gather user selection
         selection = menu()
 
-        # dictonary switch for user selection control
+        # dictswitch for user selection control
         run = defswitcher(selection, run)
 
     return 0
+
+
+def menu():
+    selection = None
+    while selection is None:
+        print(
+            '''P:  Print all inventory
+A:  Add a new part
+L:  Lookup part by number
+R:  List parts that are low on inventory
+Z:  Archive a part
+Q:  Exit/Quit '''
+        )
+
+        choose = input("Enter choice from menu above:   ")
+
+        # Check validity of user selection, rerun if invalid
+        if re.fullmatch('[P|A|L|R|Z|Q]', choose):
+            selection = choose
+            return selection
+        else:
+            print('\nPlease select a valid option')
 
 
 def defswitcher(selection, run):
@@ -30,7 +52,7 @@ def defswitcher(selection, run):
         'R': lowinv,
         'Z': archive,
     }
-
+    # exit control
     if selection is 'Q':
         run = False
     else:
@@ -44,8 +66,8 @@ def defswitcher(selection, run):
 def printall():
     # passing global to defs to avoid creating a new box for Schrödinger each run though
     global parts, descr
-
-    print("  ".join())
+    for key, value in parts.items():
+        print("  " + key + " " + value)
 
 
 def addpart():
@@ -62,25 +84,6 @@ def lowinv():
 
 def archive():
     print("archive")
-
-
-def menu():
-    print(
-        '''P:  Print all inventory
-A:  Add a new part
-L:  Lookup part by number
-R:  List parts that are low on inventory
-Z:  Archive a part
-Q:  Exit/Quit '''
-    )
-    selection = input("Enter choice from menu above:   ")
-
-    # Check validity of user selection, rerun if invalid
-    if re.fullmatch('[P|A|L|R|Z|Q]', selection):
-        return selection
-    else:
-        print('\nYou must Choose a valid option')
-        menu()
 
 
 if __name__ == "__main__":
